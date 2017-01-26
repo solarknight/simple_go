479ea8e7932e06963fedecb5cda09bf08596fe9c
solarknight <zhouthanos@gmail.com>
2017-01-26 10:47:30 +0800

    update

--- hello.go
+++ hello.go
@@ -2,10 +2,21 @@ package main
 
 import (
     "fmt"
-    "github.com/solarknight/stringutil"
 )
 
 func main() {
-    fmt.Printf("Hello, world\n")
-    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    fmt.Println("Hello, world\n")
+		beyondHello()
 }
+
+func beyondHello() {
+		var x int = 3
+		y := 4
+		sum, prod := learnMultiple(x, y)
+		fmt.Println("sum:", sum , "prod:", prod)
+}
+
+func learnMultiple(x, y int) (sum, prod int) {
+		return x + y, x * y
+}
+
