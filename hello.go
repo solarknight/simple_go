c0fffa4fb0247f257cfb7b95c8f8a79d6af64d78
solarknight <zhouthanos@gmail.com>
2017-01-27 20:46:30 +0800

    test multi line string

--- hello.go
+++ hello.go
@@ -2,9 +2,19 @@ package main
 
 import fm "fmt"
 
+// HappyNewYear is a long string
+const HappyNewYear = "Happy new year!  Happy new year! Happy new year!" +
+	"Happy new year! Happy new year!"
+
+// MultiLineHappyNewYear is a multi line long string
+const MultiLineHappyNewYear = `Happy new year! Happy new year! Happy new year! Happy new year!
+ Happy new year! Happy new year! Happy new year! `
+
 func main() {
 	fm.Println("Hello, world")
 	beyondHello()
+	printLongString(HappyNewYear)
+	printLongString(MultiLineHappyNewYear)
 }
 
 func beyondHello() {
@@ -16,4 +26,8 @@ func beyondHello() {
 
 func learnMultiple(x, y int) (sum, prod int) {
 	return x + y, x * y
+}
+
+func printLongString(s string) {
+	fm.Println(s)
 }
