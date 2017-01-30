83160267916258c1cc7fa77b066db1b7e88de0e3
solarknight <zhouthanos@gmail.com>
2017-01-30 19:24:46 +0800

    add struct

--- struct.go
+++ struct.go
@@ -11,9 +11,32 @@ type User struct {
 	sex  int
 }
 
+// Node struct
+type Node struct {
+	data float64
+	next *Node
+}
+
+// DNode double linked node
+type DNode struct {
+	head *DNode
+	data int
+	tail *DNode
+}
+
+// Tree tree node
+type Tree struct {
+	le   *Tree
+	data float64
+	ri   *Tree
+}
+
 func tryStruct() {
 	t := new(User)
 	t.name = "Tom"
 	t.age = 18
 	fmt.Println(t)
+
+	p := &User{"Cherry", 15, 1}
+	fmt.Println(p)
 }
