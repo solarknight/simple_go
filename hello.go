2e267453103a98019c72258455638ca3f201b814
solarknight <zhouthanos@gmail.com>
2017-01-30 10:39:43 +0800

    update try map

--- hello.go
+++ hello.go
@@ -50,6 +50,7 @@ func main() {
 	timeSub()
 	trySlice()
 	tryMap()
+	tryRegexp()
 }
 
 func beyondHello() {
--- map.go
+++ map.go
@@ -1,8 +1,15 @@
 package main
 
 import "fmt"
+import "sort"
 
-var mp1 = make(map[int][]int)
+var mp1 = make(map[int][]int, 100)
+
+// LinkedHashMap is important!
+type LinkedHashMap struct {
+	key   string
+	value int
+}
 
 func tryMap() {
 	var mapLit map[string]int
@@ -21,4 +28,48 @@ func tryMap() {
 	fmt.Printf("Map assigned at \"two\" is: %d\n", mapLit["two"])
 	fmt.Printf("Map literal at \"ten\" is: %d\n", mapLit["ten"])
 
+	_, isPresent := mapLit["three"]
+	if !isPresent {
+		mapLit["three"] = 5
+	}
+	_, isPresent = mapLit["three"]
+	fmt.Println("key three is present?", isPresent)
+	if _, ok := mapLit["three"]; ok {
+		delete(mapLit, "three")
+	}
+	_, isPresent = mapLit["three"]
+	fmt.Println("key three is present?", isPresent)
+
+	for key, value := range mapLit {
+		fmt.Println("key", key, "value", value)
+	}
+
+	// Version A:
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Printf("Version A: Value of items: %v\n", items)
+
+	sortMap()
+}
+
+func sortMap() {
+	barVal := map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+		"delta": 87, "echo": 56, "foxtrot": 12,
+		"golf": 34, "hotel": 16, "indio": 87,
+		"juliet": 65, "kili": 43, "lima": 98}
+
+	keys := make([]string, len(barVal))
+	i := 0
+	for key := range barVal {
+		keys[i] = key
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
+	}
 }
